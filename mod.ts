27d4b9ac469ea46da2e4d670b5767792b1ddab64
lilnasy <emmagoldmanvevo@pm.me>
2022-01-15 06:17:37 +0000

    faster algorithm, correct linear rgb conversion

--- mod.ts
+++ mod.ts
@@ -18,30 +18,36 @@ export interface Color {
  * @param hue hue angle in degrees (0 to 360)
 */
 export function LCHtoHexString ({lightness, chroma, hue}: Color): string {
-	
 	assert(0 <= lightness && lightness <= 100, 'Lightness must be between 0 and 100')
 	assert(0 <= chroma, 'Chroma must be at least 0')
-	
 	const rgb = LCHtoRGB([lightness, chroma, hue])
-	
 	return toHexString(rgb)
 }
 
-function LCHtoRGB ([lightness, chroma, hue]: number[]): number[] {
-	
-	const Δ = 0.1
-	
-	if (chroma <= 0) return [0, 0, 0]
-	
+// converts lch described colors to their rgb channel values and, if necessary, adapts colors
+// that are outside the sRGB gamut to their less chromatic counterparts that are inside.
+// the algorithm steps closer to the most chromatic color that is inside the gamut with each
+// recursion, halving the amount by which it steps each time, until it is close enough ( when
+// it is less than 0.1 units away from the actual chroma )
+function LCHtoRGB (
+        [lightness, chroma, hue]: number[],
+        step = chroma / 2,
+        adapting = false
+): number[] {
 	const rgb = sRGB(toLinearRGB(toD65(toD50(toXYZ(toLAB([lightness, chroma, hue]))))))
-	
-	// logpoint: { new Map([ [chroma, rgb] ]) }
-	
-	return rgb.every( x => x >= 0 ) ? rgb : LCHtoRGB([lightness, chroma - Δ, hue])
+	// helpful logpoint: chroma: { chroma } rgb: { rgb.map( x => Math.round( 255 * x ) )] }
+	// with this algorithm, colors very close to black and white may approach
+	// the range [0,0,0] to [1,1,1] asymptotically with each half-step without
+	// ever making it inside - so I've picked values that are outside the gamut
+	// but still small enough that they get rounded to 0 or 255
+        const inGamut = rgb.every( x => x >= -0.0001 ) && rgb.every( x => x <= 1.0001 )
+        if (inGamut && adapting === false) return rgb
+        else if (inGamut && step <= 0.1) return rgb
+        else if (inGamut) return LCHtoRGB( [lightness, chroma + step, hue], step / 2, adapting = true)
+	else return LCHtoRGB( [lightness, chroma - step, hue], step / 2, adapting = true)
 }
 
 function toLAB ([lightness, chroma, hue]: number[]): number[] {
-	
 	return [
 		lightness,
 		chroma * Math.cos(hue * Math.PI / 180),
@@ -50,25 +56,20 @@ function toLAB ([lightness, chroma, hue]: number[]): number[] {
 }
 
 function toXYZ ([lightness, a, b]: number[]): number[] {
-	
-	const κ = (3 / 29) ** 3
-	const ε = 6 / 29
-	
+	const κ = 3 / 29
 	const xyz = [
 		((lightness + 16) / 116) + (a / 500),
 		 (lightness + 16) / 116,
 		((lightness + 16) / 116) - (b / 200)
 	]
-	
-	return xyz.map( x => x > ε
+	return xyz.map( x => x > 2 * κ
 				? x ** 3
-				: κ * (x * 116 - 16) )
+				: κ ** 3 * (x * 116 - 16) )
 }
 
 /** converts xyz co-ordinates to D50-adapted xyz co-ordinates
 https://web.archive.org/web/20211025225437/http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html */
 function toD50([x, y, z]: number[]): number[] {
-	
 	return [ 
 		x * 0.3457 / 0.3585,
 		y,
@@ -78,7 +79,6 @@ function toD50([x, y, z]: number[]): number[] {
 
 // Bradford chromatic adaptation from D50 to D65
 function toD65([x, y, z]: number[]): number[] {
-	
 	return [
 		 0.955473452704218200 * x - 0.023098536874261423 * y + 0.063259308661021700 * z,
 		-0.028369706963208136 * x + 1.009995458005822600 * y + 0.021041398966943008 * z,
@@ -87,28 +87,23 @@ function toD65([x, y, z]: number[]): number[] {
 }
 
 function toLinearRGB ([x, y, z]: number[]): number[] {
-	
 	return [
-		 3.1338561 * x - 1.6168667 * y - 0.4906146 * z,
-		-0.9787684 * x + 1.9161415 * y + 0.0334540 * z,
-		 0.0719453 * x - 0.2289914 * y + 1.4052427 * z
+		 3.24096994190452260 * x - 1.53738317757009400 * y - 0.49861076029300340 * z,
+		-0.96924363628087960 * x + 1.87596750150772020 * y + 0.04155505740717559 * z,
+		 0.05563007969699366 * x - 0.20397695888897652 * y + 1.05697151424287860 * z
 	]
 }
 
 function sRGB (lrgb: number[]): number[] {
-
 	const gamma = (val: number) => {
 		const sign = val < 0 ? -1 : 1
 		const abs = Math.abs(val)
 		return (abs > 0.0031308) ? sign * (1.055 * Math.pow(abs, 1/2.4) - 0.055) : 12.92 * val
 	}
-	
-	return lrgb.map(gamma).map( x => Math.round( 255 * x ) )
+	return lrgb.map(gamma)
 }
 
 function toHexString (rgb: number[]): string {
-	
-	const [r, g, b] = rgb.map( x => x.toString(16).padStart(2, '0') )
-	
+	const [r, g, b] = rgb.map( x => Math.round( 255 * x ) ).map( x => x.toString(16).padStart(2, '0') )
 	return '#' + r + g + b
 }
